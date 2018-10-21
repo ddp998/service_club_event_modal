1186fa83a39a6ca4f6d199abb94f8aaf68a95502
Ashley Maher <ashley.maher@didymodesigns.com.au>
2018-10-21 16:52:21 +1100

    The modal has a more uniform user experience.

--- src/Controller/EventModalController.php
+++ src/Controller/EventModalController.php
@@ -13,28 +13,30 @@ use Drupal\Core\Ajax\OpenModalDialogCommand;
 use Drupal\Core\Controller\ControllerBase;
 
 class EventModalController extends ControllerBase {
-    /**
-     * @param $entityid
-     * @return AjaxResponse
-     *
-     * This function is called via the routing when the user clicks the "Open TMP" Button.
-     * It launches the modal window.
-     */
-    public function launchModal($entityid) {
-        // The #theme needs to be the name of the module.
-        $content['#theme'] = 'service_club_event_modal';
-        // The #attached library needs to be the name of the module/ the name defined in the libraries.yml.
-        $content['#attached']['library'][] = 'service_club_event_modal/event_modal';
 
-        // Set up the options for the modal popup.
-        $options = [
-            'dialogClass' => 'popup-dialog-class',
-            'width' => '80%',
-        ];
-        $response = new AjaxResponse();
-        // The first parameter is the title of the modal window.
-        $response->addCommand(new OpenModalDialogCommand(t('Event Mapping Tool'), $content, $options, '<app-root>'));
+  /**
+   * @param $entityid
+   *
+   * @return AjaxResponse
+   *
+   * This function is called via the routing when the user clicks the "Open
+   *   TMP" Button. It launches the modal window.
+   */
+  public function launchModal($entityid) {
+    // The #theme needs to be the name of the module.
+    $content['#theme'] = 'service_club_event_modal';
+    // The #attached library needs to be the name of the module/ the name defined in the libraries.yml.
+    $content['#attached']['library'][] = 'service_club_event_modal/event_modal';
 
-        return $response;
-    }
+    // Set up the options for the modal popup.
+    $options = [
+      'dialogClass' => 'traffic-modal',
+      'overflow-y'=> 'scroll',
+    ];
+    $response = new AjaxResponse();
+    // The first parameter is the title of the modal window.
+    $response->addCommand(new OpenModalDialogCommand(t('Event Mapping Tool'), $content, $options, '<app-root>'));
+
+    return $response;
+  }
 }
